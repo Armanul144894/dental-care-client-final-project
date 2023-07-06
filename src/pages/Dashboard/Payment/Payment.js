adaaa895bbedf054407dac01253f0ab7d393e9f0
Armanul144894 <armanul15-2842@diu.edu.bd>
2023-07-06 23:53:21 +0600

    payment add

--- src/pages/Dashboard/Payment/Payment.js
+++ src/pages/Dashboard/Payment/Payment.js
@@ -1,22 +1,34 @@
+import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigation } from "react-router-dom";
 
+import CheckoutForm from "./CheckoutForm";
 const stripePromise = loadStripe(
   "pk_test_51NPAOhLmrY4w4h0S48s5vgObUWt6WbjKIEgIjQfR0EXEaIvMJbOVabBhOIaNJ5gOEZ5w0GGc7YRmlkMp3gD60xbO007ivHLspI"
 );
 
 const Payment = () => {
   const booking = useLoaderData();
-  console.log(booking);
+
   return (
     <div>
-      <h1 className="text-2xl font-bold my-5 text-start">
-        Payment for{" "}
-        {booking.map((pay) => (
-          <span key={pay._id}>{pay.treatment}</span>
+      {booking.map((buy) => (
+        <div>
+          <h3 className="text-3xl">Payment for {buy.treatment}</h3>
+          <p className="text-xl">
+            Please pay <strong>${buy.price}</strong> for your appointment on{" "}
+            {buy.appointmentDate} at {buy.slots}
+          </p>
+        </div>
+      ))}
+      <div className="w-96 my-12">
+        {booking.map((booking) => (
+          <Elements stripe={stripePromise}>
+            <CheckoutForm booking={booking} />
+          </Elements>
         ))}
-      </h1>
+      </div>
     </div>
   );
 };
